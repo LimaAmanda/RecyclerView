f3d2bd4557a4d133285ad7a70649d6edb1e2c1d9
Amanda Cristina <amandacris_lima@outlook.com>
2021-11-05 21:19:01 -0300

    [Adicionado os cards]

--- app/src/main/java/com/example/affirmations2/MainActivity.kt
+++ app/src/main/java/com/example/affirmations2/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
-import com.example.affirmations.data.Datasource
+import com.example.affirmations2.data.Datasource
 
 
 
